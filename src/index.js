9418641c9576a17057d7553d86e6eb8393662a63
Alfredo Miranda <alfredo@miranda.io>
2018-08-16 15:24:41 +0100

    Configure webpack to handle CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import './styles.css';
 
-console.log('Webpack is working');
 render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: { rules: [{ test: /\.js$/, use: 'babel-loader' }] },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
   plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
 };
