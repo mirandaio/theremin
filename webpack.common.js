0146d0e6517eba6b4bbf7f9e03fbf903158f82f0
Alfredo Miranda <alfredo@miranda.io>
2018-08-30 00:17:55 +0100

    Add different webpack configurations for prod and dev

--- webpack.common.js
+++ webpack.common.js
@@ -7,10 +7,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
