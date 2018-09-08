83503401e3a6deb7ed7d13af4f3ae2b30aa0a3a3
Alfredo Miranda <alfredo@miranda.io>
2018-09-08 15:52:18 +0100

    Use a png favicon

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
-      favicon: 'src/favicon.svg'
+      favicon: 'src/favicon.png'
     })
   ]
 };
